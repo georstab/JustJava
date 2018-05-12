159600f99f522c0fa5e61bb420e49abed6201ddd
GeorStab <georstab@gmail.com>
2018-05-12 13:16:42 +0300

    3 TextView , 1 Buttom , 3 methods: Change Text for number of coffes , Change text of price , Method to run these methods from buttom

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        int numberOfCoffes=4;
+        display(numberOfCoffes);
+        displayPrice(numberOfCoffes*5);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+}
